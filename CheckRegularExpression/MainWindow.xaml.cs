a9361dd1aacd7f9fb6a9d60c91e5bfe2229a1ce0
Dmitriy Bobrovskiy <apollondiamed@gmail.com>
2016-12-02 23:36:05 +0500

    Initialize
    Initialize repository

---
+++ CheckRegularExpression/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace CheckRegularExpression
+{
+	public partial class MainWindow : Window
+	{
+		HttpClient httpClient;
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			Dispatcher.UnhandledException += Dispatcher_UnhandledException;
+
+            httpClient = new HttpClient();
+		}
+
+		private void Dispatcher_UnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
+			MessageBox.Show(e.Exception.Message, "Ошибка");
+			//return;
+		}
+
+		private void BtnCheck_ClickAsync(object sender, RoutedEventArgs e)
+		{
+			FillResults();
+		}
+
+		private void tbUrl_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+			if (e.Key == System.Windows.Input.Key.Enter)
+			{
+				FillResults();
+			}
+		}
+
+		private async void FillResults()
+		{
+			string responseBodyAsText;
+			Uri uri = new Uri(tbUrl.Text);
+
+			HttpResponseMessage response = await httpClient.GetAsync(uri.ToString());
+			response.EnsureSuccessStatusCode();
+
+			responseBodyAsText = await response.Content.ReadAsStringAsync();
+			Regex reg = new Regex(tbPattern.Text);
+			MatchCollection matches = reg.Matches(responseBodyAsText);
+
+			foreach (var m in matches)
+			{
+				tbResultText.Text += m.ToString() + "\n";
+			}
+		}
+	}
+}
---
+++ CheckRegularExpression/Uri.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckRegularExpression
+{
+    internal class Uri
+    {
+		private string _address;
+		public string Address
+		{
+			get	{ return _address; }
+			private set
+			{
+				_address = value.Contains("http://") ? value : string.Concat("http://", value);
+			}
+		}
+
+		public Uri(string address)
+		{
+			Address = address;
+		}
+
+		public override string ToString()
+		{
+			return Address;
+		}
+	}
+}
